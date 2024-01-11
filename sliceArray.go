1d6a7af5e4467097a3fadb0f1e6cb163810f92c4
andriyanto <andriyan354354@gmail.com>
2024-01-11 23:36:48 +0700

    Update sliceArray.go
    testing slice dengan append

--- sliceArray.go
+++ sliceArray.go
@@ -18,4 +18,21 @@ func main() {
 	fmt.Println(fmt.Sprintf("slice 1 %s", slice1))
 	slice1[0] = "NIHIL"
 	fmt.Println(slice1)
+
+	var days = [...]string{
+		"senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu",
+	}
+
+	fmt.Println(days)
+	daysSlice := days[6:]
+	daysSlice[0] = "test aja sih"
+
+	daysSlice2 := append(daysSlice, "Libur")
+	daysSlice2[0] = "HMMMMM"
+
+	fmt.Println(fmt.Sprintf("slice 1 : %s", daysSlice))
+	fmt.Println(fmt.Sprintf("slice 2 : %s", daysSlice2))
+
+	fmt.Println(fmt.Sprintf("from : %s change into %s", days[6:], daysSlice))
+
 }
