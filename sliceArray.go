31566cabdb1103890499e4b5a111247d91385430
andriyanto <andriyan354354@gmail.com>
2024-05-04 11:26:15 +0700

    slice

--- sliceArray.go
+++ sliceArray.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// fun convertStringToInt(values : *strconv.T): string{
+// 	num, err :=  strconv.Atoi(values)
+
+// 	if err != nil {
+// 		fmt.Println("Error ", err)
+// 		return "0"
+// 	}
+
+// 	fmt.Println("success convert!!")
+// 	return num
+// }
 
 func main() {
 	// var months = [...]string{
@@ -19,20 +33,54 @@ func main() {
 	// slice1[0] = "NIHIL"
 	// fmt.Println(slice1)
 
-	var days = [...]string{
-		"senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu",
+	// var days = [...]string{
+	// 	"senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu",
+	// }
+
+	// fmt.Println(days)
+	// daysSlice := days[3:]
+	// // daysSlice[0] = "test aja sih"
+
+	// daysSlice2 := append(daysSlice, "Libur")
+	// daysSlice2[0] = "HMMMMM"
+
+	// fmt.Println(len(daysSlice))
+	// fmt.Println(fmt.Sprintf("slice 1 : %s", daysSlice))
+	// fmt.Println(fmt.Sprintf("slice 2 : %s", daysSlice2))
+	// fmt.Println(fmt.Sprintf("Slince length : %s\n cap : %s", len(daysSlice), cap(daysSlice)))
+
+	// fmt.Println(fmt.Sprintf("from : %s change into %s", days[6:], daysSlice))
+
+	// fmt.Println("result convert : ", convertStringToInt("12"))
+
+	var months = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
 	}
 
-	fmt.Println(days)
-	daysSlice := days[3:]
-	daysSlice[0] = "test aja sih"
+	fmt.Println(months)
 
-	daysSlice2 := append(daysSlice, "Libur")
-	daysSlice2[0] = "HMMMMM"
+	daysSlice1 := months[5:]
+	daysSlice1[0] = "Juni summer"
+	daysSlice1[3] = "September dumn"
 
-	fmt.Println(fmt.Sprintf("slice 1 : %s", daysSlice))
-	fmt.Println(fmt.Sprintf("slice 2 : %s", daysSlice2))
+	fmt.Println(daysSlice1)
 
-	fmt.Println(fmt.Sprintf("from : %s change into %s", days[6:], daysSlice))
+	daysSlice2 := append(daysSlice1, "April Mop")
+	fmt.Println(daysSlice2)
+
+	daysSlice2[0] = "Hari libur!!"
+
+	fmt.Println(daysSlice2)
 
 }
