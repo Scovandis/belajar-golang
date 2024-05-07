b4b17e32d001607134a85cce93339932d09a6fb9
andriyanto <andriyan354354@gmail.com>
2024-05-07 18:44:42 +0700

    func as params

--- funcAsParams.go
+++ funcAsParams.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 )
 
+type Filter func(string) string
+
 func main() {
 	fmt.Println("Hallo")
 	sayWithHelo("DOG", spamFilter)
 	sayWithHelo("Andre", spamFilter)
+	fmt.Println(sayAja("Andre", spamFilter))
 }
 
-func sayWithHelo(name string, filter func(string) string) {
+func sayWithHelo(name string, filter Filter) {
 	fmt.Println("Hallo ", filter(name))
+}
+
+func sayAja(name string, filter func(string) string) string {
+	return fmt.Sprintf("SAY AJA YAHHH>>> %s", filter(name))
 }
 
 func spamFilter(name string) string {
