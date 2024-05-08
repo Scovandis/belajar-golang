9fd461e390228c2ece6aca58536f97bca3aaea8e
Andriyanto <50163581+Scovandis@users.noreply.github.com>
2024-05-08 13:32:57 +0700

    shotcut multipe comment

---
+++ komentar.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Hello word")
+
+	// single line comment
+	
+	/* 
+	VScode -- ini multype comment --
+
+	-Windows: Shift+Alt+A
+	-Linux: Shift+Ctrl+A
+	-MacOS: shift+option+A
+	 */
+
+
+}
